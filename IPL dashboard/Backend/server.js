8704b4eeb0bce68cfd7e7eef3144ee83d09f7701
Satyam Srivastav <162419614+satyam22i@users.noreply.github.com>
2025-07-25 05:18:49 +0530

    Update server.js

--- IPL dashboard/Backend/server.js
+++ IPL dashboard/Backend/server.js
@@ -12,11 +12,12 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://frontendipl.onrender.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
 }));
+
 app.use(express.json());
 
 
