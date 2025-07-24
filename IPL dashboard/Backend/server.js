224d9ecfbf26eea8dd6412040f5a108dfad01425
Satyam Srivastav <162419614+satyam22i@users.noreply.github.com>
2025-07-25 03:29:01 +0530

    Update server.js

--- IPL dashboard/Backend/server.js
+++ IPL dashboard/Backend/server.js
@@ -4,13 +4,13 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import matchRoutes from "./routes/rotes.js";
 
-dotenv.config(); // Load .env variables
+dotenv.config(); 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-// Middleware
+
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true,
@@ -19,13 +19,12 @@ app.use(cors({
 }));
 app.use(express.json());
 
-// Connect to MongoDB Atlas
+
 connectDB();
 
-// Routes
+
 app.use("/api/matches", matchRoutes);
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
