b849ac3cfed69c28e6716b6f58fc8c821cc57c25
Satyam Srivastav <162419614+satyam22i@users.noreply.github.com>
2025-07-25 15:31:20 +0530

    Update server.js

--- IPL dashboard/Backend/server.js
+++ IPL dashboard/Backend/server.js
@@ -11,12 +11,24 @@ const PORT = process.env.PORT || 5000;
 
 
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://fluffy-cupcake-9ebe78.netlify.app'
+];
+
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
 }));
+
 
 app.use(express.json());
 
